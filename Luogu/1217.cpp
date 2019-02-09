c077103cfbbd6bcfb7303820f48b514ba857764d
L-Trump <yinghaochi@163.com>
2019-02-09 22:05:59 +0800

    PASS

--- Luogu/1217.cpp
+++ Luogu/1217.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <cstring>
 #include <algorithm>
-#define NMAX 100000000
+#define NMAX 10000000
 #define rep(i, l, r) for (int i = (l); i <= (r); ++i)
 #define per(i, l, r) for (int i = (l); i >= (r); --i)
 using std::cin; using std::cout; using std::cerr; using std::endl; using std::string;
 typedef long long ll;
-bool vis[NMAX+5]={};
+bool vis[NMAX+5]={},isPrime[NMAX+5]={};
 int prime[NMAX+5]={};
 bool isHui(int a){
     int b=a, t=0;
@@ -28,16 +28,18 @@ int main(){
     //cout<<isHui(121)<<endl;
     //cerr<<isHui(3)<<endl;
     cin>>a>>b;
+    if(b>10000000) b=10000000;
     rep(i,2,b){
-      if(!vis[i]) {prime[++cnt]=i;}
+      if(!vis[i]) {prime[++cnt]=i;isPrime[i]=1;}
       for(int j=1;j<=cnt&&i*prime[j]<=b;j++){
         vis[i*prime[j]]=true;
         if(i%prime[j]==0) break;
       }
     }
-    rep(i,1,cnt){
+    rep(i,a,b){
       //cerr<<"prime: "<<prime[i]<<'\n';
-      if(isHui(prime[i])&&prime[i]>=a) cout<<prime[i]<<endl;
+      if(i>NMAX) break;
+      if(isHui(i)&&isPrime[i]) cout<<i<<'\n';
     }
     return 0;
 }
