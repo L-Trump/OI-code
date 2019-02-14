81a4ce6018fd8550d404f062b3be2a6e2d286156
L-Trump <yinghaochi@163.com>
2019-02-14 22:14:21 +0800

    建图

--- Luogu/1038.cpp
+++ Luogu/1038.cpp
@@ -4,12 +4,37 @@
 using namespace std;
 typedef long long ll;
 typedef unsigned int ui;
-int main(){
 
+struct node{
+    int v,u;
+    queue<pair<node,int>> edge;
+    node(){
+      v=u=0;
+    }
+} e[105];
+
+queue<int> root;
+
+inline void createEdge(node a,node b,int w){
+    a.edge.push(make_pair(b,w));
+    return;
+}
+int main(){
+    int n,p;
     //freopen("triangle.in", "r", stdin);
     //freopen("triangle.out", "w", stdout);
     std::ios::sync_with_stdio(false);
     cout.tie(0);
-
+    scanf("%d%d",&n,&p);
+    rep(i,1,n){
+      scanf("%d%d",&e[i].v,&e[i].u);
+      if(e[i].v!=0) root.push(i);
+    }
+    rep(i,1,p){
+      int a,b,w;
+      scanf("%d%d%d",&a,&b,&w);
+      createEdge(e[a],e[b],w);
+      createEdge(e[b],e[a],w);
+    }
     return 0;
 }
