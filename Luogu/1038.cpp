9a808198204a7b3965ca3044218f568580553d44
L-Trump <yinghaochi@163.com>
2019-02-14 21:55:11 +0800

    create 1038

---
+++ Luogu/1038.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, l, r) for (int i = (l); i <= (r); ++i)
+#define per(i, l, r) for (int i = (l); i >= (r); --i)
+using namespace std;
+typedef long long ll;
+typedef unsigned int ui;
+int main(){
+
+    //freopen("triangle.in", "r", stdin);
+    //freopen("triangle.out", "w", stdout);
+    std::ios::sync_with_stdio(false);
+    cout.tie(0);
+
+    return 0;
+}
