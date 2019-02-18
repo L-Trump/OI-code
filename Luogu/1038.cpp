777b6fe2cdcf6c6857146b7ab7b1683d204aaf82
L-Trump <yinghaochi@163.com>
2019-02-18 22:07:38 +0800

    完成四个点，一点WA

--- Luogu/1038.cpp
+++ Luogu/1038.cpp
@@ -6,21 +6,74 @@ typedef long long ll;
 typedef unsigned int ui;
 
 struct node{
-    int v,u;
-    queue<pair<node,int>> edge;
+    int v,u,in,out;
+    bool vis;
+    vector<pair<int,int> > edge;
     node(){
       v=u=0;
+      in=out=0;
+      vis=false;
     }
 } e[105];
 
 queue<int> root;
+priority_queue<int,vector<int>,greater<int> > last;
 
-inline void createEdge(node a,node b,int w){
-    a.edge.push(make_pair(b,w));
+inline void createEdge(int a,int b,int w){
+    e[a].edge.push_back(make_pair(b,w));
     return;
 }
+
+bool isExisted(queue<int> q,int a){
+    while(!q.empty()){
+      if(a==q.front()) return true;
+      q.pop();
+    }
+    return false;
+}
+
+void bfs1(){
+    queue<int> q=root;
+    int t;
+    while(!q.empty()){
+      t=q.front();q.pop();
+      e[t].vis=true;
+      e[t].v-=e[t].u;
+      rep(i,0,(int)e[t].edge.size()-1){
+        int nxt=e[t].edge[i].first;
+        int wid=e[t].edge[i].second;
+        if(!e[nxt].vis){
+          e[t].out++;
+          if(!isExisted(q,nxt)) q.push(nxt);
+          e[nxt].in++;
+          if(e[t].v>0) e[nxt].v+=e[t].v*wid;
+        }
+      }
+      if(e[t].out==0) last.push(t);
+    }
+}
+/*
+void bfs2(){
+    queue<int> q=root;
+    int t;
+    while(!q.empty()){
+      t=q.front();q.pop();
+      e[t].vis=true;
+      //e[t].v-=e[t].u;
+      rep(i,0,(int)e[t].edge.size()-1){
+        int nxt=e[t].edge[i].first;
+        if(!e[nxt].vis){
+          e[t].out++;
+          if(!isExisted(q,nxt)) q.push(nxt);
+          e[nxt].in++;
+        }
+      }
+      if(e[t].out==0) last.push(t);
+    }
+}
+*/
 int main(){
-    int n,p;
+    int n,p,t;
     //freopen("triangle.in", "r", stdin);
     //freopen("triangle.out", "w", stdout);
     std::ios::sync_with_stdio(false);
@@ -28,13 +81,24 @@ int main(){
     scanf("%d%d",&n,&p);
     rep(i,1,n){
       scanf("%d%d",&e[i].v,&e[i].u);
-      if(e[i].v!=0) root.push(i);
+      if(e[i].v!=0){
+        root.push(i);
+        e[i].u=0;
+      }
     }
     rep(i,1,p){
       int a,b,w;
       scanf("%d%d%d",&a,&b,&w);
-      createEdge(e[a],e[b],w);
-      createEdge(e[b],e[a],w);
+      createEdge(a,b,w);
+      createEdge(b,a,w);
+    }
+    bfs1();
+    //rep(i,1,n) e[i].vis=false;
+    while(!last.empty()){
+      t=last.top();
+      last.pop();
+      if(e[t].v>0)
+        printf("%d %d\n",t,e[t].v);
     }
     return 0;
 }
