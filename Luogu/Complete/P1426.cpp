db10166402f3354d8bdc8bb616f03074d4e125b8
L-Trump <yinghaochi@163.com>
2019-03-02 21:52:24 +0800

    Finish P1426

--- Luogu/Complete/P1426.cpp
+++ Luogu/Complete/P1426.cpp
@@ -7,11 +7,20 @@
 using std::cin; using std::cout; using std::cerr; using std::endl; using std::string;
 typedef long long ll;
 int main(){
-
+    double s,x;
+    double dis=7,pos=0;
     //freopen("triangle.in", "r", stdin);
     //freopen("triangle.out", "w", stdout);
     std::ios::sync_with_stdio(false);
     cout.tie(0);
-    
+    cin>>s>>x;
+    while(pos<s-x){
+      pos+=dis;
+      dis*=0.98;
+    }
+    pos+=dis*0.98;
+    if(pos<s+x)cout<<"y";
+    else cout<<"n";
+    cout<<endl;
     return 0;
 }
