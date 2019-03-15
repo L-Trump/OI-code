fb93a12d84c6d97d17445ce80c521f539940e1cd
L-Trump <yinghaochi@163.com>
2019-03-15 20:48:12 +0800

    Finish Holes, Score 100

--- 模拟赛/20190309/holes/holes.cpp
+++ 模拟赛/20190309/holes/holes.cpp
@@ -10,95 +10,94 @@
 #define MMAX 30000
 using namespace std;
 typedef long long ll;
+int n;
 struct hole{
-    bool isBlack;
     int w;
     int dis;
     hole(){
-        w=isBlack=0;
+        w=0;
         dis=1000000007;
     }
 } h[2*NMAX+5];
 struct edge{
     int to;
-    int waste;
+    int w;
 };
 vector<edge> e[2*NMAX+5];
 inline void createEdge(int a, int b, int s){
     edge c;
     c.to=b;
-    c.waste=s;
+    c.w=s;
     e[a].push_back(c);
 }
-bool isExist(int a, queue<int> t){
-    int temp;
-    while(!t.empty()){
-        temp=t.front();
-        t.pop();
-        if(temp==a) return true;
-    }
-    return false;
-}
-ll spfa(int root,int last){
-    queue<int> q1,q2,emptyq;
-    int now=1;
-    q1.push(root);
+
+void spfa(int root){
+    queue<int> q;
+    rep(i,1,2*n+3) h[i].dis=1000000007;
     h[root].dis=0;
-    while(!q1.empty()){
-        while(!q1.empty()){
-            root=q1.front();q1.pop();
-            //cerr<<"root is "<<root<<endl;
-            //cerr<<"has edges number "<<e[root].size()-1<<endl;
-            rep(i,0,(int)e[root].size()-1){
-                int waste=e[root][i].waste;
-                int to=e[root][i].to;
-               // cerr<<root<<" has edges to "<<to<<endl;
-                int delta=abs(h[to].w-h[root].w)*(h[root].isBlack-h[to].isBlack)*now;
-               // cerr<<"delta is "<<delta<<endl;
-                waste+=delta;
-                if(waste<0) waste=0;
-               // cerr<<"waste is "<<waste<<endl;
-                if(h[to].dis>waste+h[root].dis){
-                    //if(!isExist(to,q2)){
-                        q2.push(to);
-                 //       cerr<<to<<" has pushed to q2"<<endl;
-                    //}
-                    h[to].dis=waste+h[root].dis;
-                   // cerr<<"dis to "<<to<<" become "<<waste+h[root].dis<<endl;
+    bool isInQ[2*n+5]={};
+    q.push(root);
+    isInQ[root]=1;
+    while(!q.empty()){
+        root=q.front();q.pop();
+
+        //cerr<<"root is "<<root<<endl;
+        //cerr<<"has edges number "<<e[root].size()-1<<endl;
+
+        isInQ[root]=0;
+        rep(i,0,(int)e[root].size()-1){
+            int to=e[root][i].to;
+            int w=e[root][i].w;
+          //  cerr<<"has edge "<<to<<" "<<w<<endl;
+
+          //  cerr<<"old value is "<<h[to].dis<<endl;
+            if(h[root].dis+w<h[to].dis){
+                h[to].dis=h[root].dis+w;
+            //    cerr<<"distance of "<<to<<" update to "<<h[to].dis<<endl;
+                if(!isInQ[to]){
+                    q.push(to);
+              //      cerr<<to<<" has pushed to q"<<endl;
+                    isInQ[to]=1;
                 }
             }
         }
-        now*=-1;
-        q1=q2;
-        q2=emptyq;
-        //cerr<<q1.size()<<endl;
+       // cerr<<endl;
     }
-    return h[last].dis;
+    //return min(h[2*n-1].dis,h[2*n].dis);
 }
 int main(){
     std::ios::sync_with_stdio(false);
     cout.tie(0);
     freopen("holes.in","r",stdin);
     freopen("holes.out","w",stdout);
-    int n,m,stay;
+    bool isBlack[NMAX+5];
+    int m,stay;
+    int u,v,k,delta;
     cin>>n>>m;
+    rep(i,1,n)
+        cin>>isBlack[i];
+        //h[2*NMAX-i].isBlack=h[i].isBlack;
     rep(i,1,n){
-        cin>>h[i].isBlack;
-        h[2*NMAX-i].isBlack=h[i].isBlack;
-    }
-    rep(i,1,n){
-        cin>>h[i].w;
-        h[2*NMAX-i].w=h[i].w;
+        cin>>h[2*i-1].w;
+        h[2*i]=h[2*i-1];
     }
     rep(i,1,n){
         cin>>stay;
-        createEdge(i,2*NMAX-i,stay);
+        createEdge(2*i-1,2*i,stay); //2*i-1为i的黑状态，2*i是白
+        createEdge(2*i,2*i-1,0);
     }
     rep(i,1,m){
-        int u,v,k;
         cin>>u>>v>>k;
-        createEdge(u,v,k);
-        createEdge(2*NMAX-u,v,k);
+        if(isBlack[u]==isBlack[v]){
+            createEdge(2*u-1,2*v,k);
+            createEdge(2*u,2*v-1,k);
+        } else {
+            delta=abs(h[2*u].w-h[2*v].w);
+            createEdge(2*u-1,2*v-1,k+delta);
+            createEdge(2*u,2*v,(k-delta>0 ? k-delta : 0));
+        }
     }
-    cout<<spfa(1,n)<<endl;
+    int start=isBlack[1]?1:2;
+    spfa(start);
+    cout<<min(h[2*n].dis,h[2*n-1].dis)<<endl;
 }
